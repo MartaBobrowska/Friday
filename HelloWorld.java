a9a048d78111f3397d79c369be1d03ec2c1e06ff
Marta <61701185+MartaBobrowska@users.noreply.github.com>
2021-02-05 14:36:50 +0000

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Marta Bobrowska
+//T00213911
+// Prints Hello World on screen
+
+public class HelloWorld{
+	public static void main (String[] args){
+		System.out.println("Hello World!!!");
+	}
+}
